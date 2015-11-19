175bfa0d10a9f1f7a6a6cb15fcbeca9c280e2acd
adf5051 <adf5051@rit.edu>
2015-11-18 20:01:45 -0500

    saving work periodically due to power outages

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ var mid = require('./middleware');
 
 var router = function (app) {
     app.get('/login', controllers.Account.loginPage);
+    app.get('/signup', controllers.Account.signupPage);
     app.get('/', controllers.Account.loginPage);
 };
 
